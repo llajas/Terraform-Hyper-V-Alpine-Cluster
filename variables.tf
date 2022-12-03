34f0602b584c4b49d1e7be33d69f4157745bb38e
Lauren Ruelas-Lajas <llajas@ibm.com>
2022-12-03 17:53:29 -0600

    Custom homelab conig

--- variables.tf
+++ variables.tf
@@ -1,26 +1,30 @@
-variable "leader_server_count" {
-  description = "How many leader instances should be created?"
+variable "vmcount0" {
+  description = "Please enter the amount of VM's you would like to have on host 1"
   type        = number
 
   validation {
-    condition     = var.leader_server_count >= 1 && var.leader_server_count <= 3 && floor(var.leader_server_count) == var.leader_server_count
-    error_message = "Please choose a number between 1-3!"
+    condition     = var.vmcount0 >= 1 && var.vmcount0 <= 27 && floor(var.vmcount0) == var.vmcount0
+    error_message = "Please choose a number between 1-27!"
   }
 }
-variable "worker_server_count" {
-  description = "How many worker instances should be created?"
+variable "vmcount1" {
+  description = "Please enter the amount of VM's you would like to have on host 2"
   type        = number
 
   validation {
-    condition     = var.worker_server_count >= 1 && var.worker_server_count <= 27 && floor(var.worker_server_count) == var.worker_server_count
+    condition     = var.vmcount1 >= 1 && var.vmcount1 <= 27 && floor(var.vmcount1) == var.vmcount1
     error_message = "Please choose a number between 1-27!"
   }
 }
 variable "switch_name" {
   type = string
-  default = "k8s DMZ"
+  default = "k8s-net"
 }
-variable "network_adapter" {
+variable "network_adapter0" {
+  description = "Please enter the name of the NIC to be bound/bridged to your virtual switch:"
+  type = string
+}
+variable "network_adapter1" {
   description = "Please enter the name of the NIC to be bound/bridged to your virtual switch:"
   type = string
 }
@@ -33,7 +37,11 @@ variable "HYPERV_PASSWORD" {
   sensitive = true
   type = string
 }
-variable "HYPERV_HOST" {
+variable "HYPERV_HOST0" {
+  description = "Enter the name (Non-FQDN) of the Windows host"
+  type = string  
+}
+variable "HYPERV_HOST1" {
   description = "Enter the name (Non-FQDN) of the Windows host"
   type = string  
 }
@@ -48,7 +56,11 @@ variable "base_image_path" {
   description = "Please specify the '.vhdx' file that will be cloned for all nodes"
   type = string
 }
-variable "working_path" {
+variable "working_path0" {
+  description = "Please specify the location used to store all created '.vhdx' files for all nodes"
+  type = string
+}
+variable "working_path1" {
   description = "Please specify the location used to store all created '.vhdx' files for all nodes"
   type = string
 }
